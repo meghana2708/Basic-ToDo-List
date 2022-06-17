7e7e9df1d9b586ba93b4a19519ac8e2b6c37cdd8
Sai Meghana Vemula <meghana.bamr@gmail.com>
2022-06-17 18:20:38 +0530

    var to const

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var label = document.querySelector('#gameplay')
-var body = document.querySelector('body')
-var bg = document.querySelector('.td')
-var toDo = document.querySelector('.addT')
-var click = document.querySelector('.add')
-var addtask = document.querySelector('.add_Task')
-var input = document.querySelector('.addtext')
-var task = document.querySelector('.addtask')
-var ul = document.querySelector('.ul')
-var button = document.querySelector('.listItems')
-var list = document.querySelector('.list')
+const label = document.querySelector('#gameplay')
+const body = document.querySelector('body')
+const bg = document.querySelector('.td')
+const toDo = document.querySelector('.addT')
+const click = document.querySelector('.add')
+const addtask = document.querySelector('.add_Task')
+const input = document.querySelector('.addtext')
+const task = document.querySelector('.addtask')
+const ul = document.querySelector('.ul')
+const button = document.querySelector('.listItems')
+const list = document.querySelector('.list')
 
 
 
@@ -47,18 +47,18 @@ const addListItems = (event) => {
         else{
         chores.push(inputValue)
         console.log(chores)
-        var element1 = document.createElement('p')
-        var wrap = document.createElement('div')
+       const element1 = document.createElement('p')
+       const wrap = document.createElement('div')
         element1.classList.add('list')
-        var TextNode = document.createTextNode(inputValue)
+       const TextNode = document.createTextNode(inputValue)
         element1.appendChild(TextNode)
         wrap.appendChild(element1)
         
         wrap.classList.add('ul')
-        var element2 = document.createElement('button')
-        var element3 = document.createElement('button')
-        var buttontext = document.createTextNode(text)
-        var done = document.createTextNode(workDone)
+        const element2 = document.createElement('button')
+        const element3 = document.createElement('button')
+       const buttontext = document.createTextNode(text)
+       const done = document.createTextNode(workDone)
         element2.classList.add('listItems')
         element3.classList.add('tick')
         element2.appendChild(buttontext)
@@ -69,15 +69,15 @@ const addListItems = (event) => {
         input.value = ""
         element2.addEventListener('click',() => {
             wrap.parentNode.removeChild(wrap)
-            var content = element1.innerHTML
+           const content = element1.innerHTML
             console.log(content)
-            var index = chores.indexOf(element1.innerHTML)
+           const index = chores.indexOf(element1.innerHTML)
             chores.splice(index,index+1)
             console.log(chores)
             
         })
         element3.addEventListener('click',() => {
-            var completedTasks =element1.innerHTML
+           const completedTasks =element1.innerHTML
             element1.innerHTML = completedTasks +" "+ "(Finished)"
             element3.parentNode.removeChild(element3)
             
